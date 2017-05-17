c9a9e560dbce51f5c0f3acd28685e5ab00b6c2bc
rivermanvr <vrgithub@riversconsulting.com>
2017-05-17 13:04:08 -0400

    refactoring code

--- game.js
+++ game.js
@@ -1,82 +1,112 @@
-var gameOfLife = {
+const gameOfLife = {
+  resetState: function() {
+    console.log('........reset state.......');
+    this.stateObj = {blueColor: {}, redColor: {}, gameCount: 0,
+      rows: (document.getElementById('settings_rows').value) * 1,
+      columns: (document.getElementById('settings_columns').value) * 1,
+      stepInterval: (document.getElementById('settings_interval').value) * 1,
+      intervalId: null
+    }
+    this.stateObj.ColorId = document.getElementById('selectColor').value;
+  },
 
-  width: (document.getElementById('settings_columns').value) * 1,
-  height: (document.getElementById('settings_rows').value) * 1,
-  stepInterval: (document.getElementById('settings_interval').value) * 1,
-  intervalId: null,
+  buildTable: function() {
+    let goltable = document.createElement('tbody');
+    // build Table HTML
+    let tablehtml = '';
+    for (let row = 0; row < this.stateObj.rows; row++) {
+      tablehtml += "<tr id='row+" + row + "'>";
+      for (let column = 0; column < this.stateObj.columns; column++) {
+        tablehtml += "<td data-status='dead' id='" + row + '-' + column + "'></td>";
+      }
+      tablehtml += '</tr>';
+    }
+    goltable.innerHTML = tablehtml;
+    return goltable;
+  },
+
+  newGame: function() {
+    let gameOver = document.getElementById('game_over');
+    gameOver.className = 'hidden disabled';
+    let clearBoard = document.getElementById('board');
+    while (clearBoard.lastChild) {
+      clearBoard.removeChild(clearBoard.lastChild);
+    }
+    this.resetState();
+    this.createAndShowBoard();
+    this.setupBoardEvents();
+  },
 
   createAndShowBoard: function () {
-    console.log('.....entering create & show board........');
     let board = document.getElementById('board');
+    if (board.childNodes.length) board.removeChild();
     board.appendChild(this.buildTable());
-    this.setupBoardEvents();
-    console.log('.....leaving create & show board........');
   },
 
   setupBoardEvents: function() {
-    console.log('.......entering setup board events......');
     this.processTable(function(cell) {
-      cell.addEventListener('click', gameOfLife.cellStatus)
+      cell.addEventListener('click', this.cellStatus)
     });
-    document.getElementById('step_btn').onclick = this.step.bind(this);
+    // -------------------------------------------------
+    //document.getElementById('step_btn').onclick = this.step.bind(this);
+    document.getElementById('step_btn').addEventListener('click', this.step.bind(this));
+    // -------------------------------------------------
     document.getElementById('autoplay_btn').onclick = this.enableAutoPlay.bind(this);
     document.getElementById('pause_btn').onclick = this.pause.bind(this);
     document.getElementById('random_btn').onclick = this.createRandom.bind(this);
-    document.getElementById('new_game_btn').onclick = this.newGame.bind(this);
+    document.getElementById('new_game_btn').onclick = this.newGame.bind(this);              //tested <--
     document.getElementById('advanced_default_btn').onclick = this.advancedDef.bind(this);
-    document.getElementById('advanced_settings_btn').onclick = this.advancedSav.bind(this);
-    console.log('.......leaving board events......');
+    document.getElementById('selectColor').onchange = this.selectColor.bind(this);
+    document.getElementById('advanced_settings_btn').onclick = this.advSettings.bind(this);
   },
 
-  cellStatus: function(ev, organism = 'blue') {
-    if (this.dataset.status === 'dead') gameOfLife.cellAlive(this, organism);
-    else gameOfLife.cellDead(this);
-  },
-
-  cellAlive: function(cell, organism) {
-      cell.className = `alive ${organism}`;
-      cell.dataset.status = `alive`;
-  },
-
-  cellDead: function(cell) {
-    cell.className = 'dead';
-    cell.dataset.status = 'dead';
-  },
-
-  processTable: function(fn, stateObj){
-    console.log('.......in processTable...........')
-    if (stateObj) stateObj.gameCount = 0;
-    for (var ht = 0; ht < this.height; ht++) {
-      for (var wd = 0; wd < this.width; wd++) {
-        let cell = document.getElementById(`${ht}-${wd}`);
-        fn(cell, ht, wd, stateObj);
+  processTable: function(fn){
+    for (let row = 0; row < this.stateObj.rows; row++) {
+      for (let column = 0; column < this.stateObj.columns; column++) {
+        fn.bind(this)(document.getElementById(`${row}-${column}`), row, column);
       }
     }
-    console.log('.......leaving processTable...........')
   },
 
-  processRules: function(cell, row, column, stateObj) {
-    console.log('in processRules...........')
-    /* rules/actions for each cell:
-          is it alive?, (red or blue?), then
-          ...Add a count to keys as follows:
-            key = cellId: 'X-Y', 0 <= X < height, 0 <= Y < width
-            do calculations and add 1 to each derived count:
-              - if red, add to redOrganism, blue then to blueOrganism
-              - cells: (x-1)(y-1), (x-1)(y), (x-1)(y+1),
-                       (x+1)(y-1), (x+1)(y), (x+1)(y+1),
-                       (x)(y-1),   (x)(y+1)
-    */
+  cellStatus: function() {
+    if (this.dataset.status === 'dead') gameOfLife.setStatus(this, 'alive', organism = 'blue');
+    else gameOfLife.setStatus(this, 'dead', 'dead');
+  },
+
+  setStatus: function(cell, status, color) {
+    console.log('..set cell status..')
+    if (color === 1) color = 'blue';
+    if (color === 2) color = 'red';
+    cell.setAttribute('data-status', status);
+    cell.className = color;
+    console.log(cell);
+  },
+
+  step: function () {
+    this.processTable(this.countNeighbors);
+    console.log('redColor: ', this.stateObj.redColor);
+    console.log('blueColor: ', this.stateObj.blueColor);
+    this.processTable(this.processNewState);
+    if (!this.stateObj.gameCount) {
+      let game = document.getElementById('game_over');
+      game.className = '';
+      this.pause();
+    }
+    this.resetState();
+    console.log('.leaving step function.........');
+  },
+
+  countNeighbors: function(cell, row, column) {
     if (cell.dataset.status !== 'dead') {
-      let orgColor = (cell.className === 'alive blue') ? 'blueOrganism' : 'redOrganism';
-      for (let rr = -1; rr < 2; rr++) {
-        let buddyRow = row + rr;
-        if (buddyRow >= 0 && buddyRow < gameOfLife.height) {
-          for (let cc = -1; cc < 2; cc++) {
-            let buddyCol = column + cc;
+      let orgColor = (cell.className === 'blue') ? 'blueColor' : 'redColor';
+      for (let rowAdjust = -1; rowAdjust < 2; rowAdjust++) {
+        let buddyRow = row + rowAdjust;
+        if (buddyRow >= 0 && buddyRow < this.stateObj.rows) {
+          for (let colAdjust = -1; colAdjust < 2; colAdjust++) {
+            let buddyCol = column + colAdjust;
             let buddyId = `${buddyRow}-${buddyCol}`;
-            if (buddyCol >= 0 && buddyCol < gameOfLife.width && buddyId !== cell.id) {
-              stateObj[orgColor][buddyId] = (stateObj[orgColor][buddyId ] + 1) || 1;
+            if (buddyCol >= 0 && buddyCol < this.stateObj.columns && buddyId !== cell.id) {
+              this.stateObj[orgColor][buddyId] = (this.stateObj[orgColor][buddyId ] + 1) || 1;
             }
           }
         }
@@ -84,82 +114,90 @@ var gameOfLife = {
     }
   },
 
-  processNewState: function(cell, row, column, stateObj) {
-    console.log('in processNewState.......');
-    /* move board to the :
-            Check both red & blue tables, result goes to tickState:
-              count (red + blue) > 3 cell dies.
-              count (red <2 && blue < 2) cell dies
-              count ( 2<= red <=3 && blue <2) red alive.
-              count ( 2<= blue <=3 && red <2) blue alive.
-              else whichever color greater is alive.
-    */
-    let countRed = stateObj.redOrganism[cell.id] || 0;
-    let countBlue = stateObj.blueOrganism[cell.id] || 0;
-    if (cell.dataset.status !== 'dead') {
-      let redAlive = countRed >= 2 && countRed <= 3;
-      let blueAlive = countBlue >= 2 && countBlue <= 3;
-      let countTotal = countRed + countBlue;
-      if (countTotal > 3 || (countRed < 2 && countBlue < 2)) {
-        this.gameOfLife.cellDead(cell);
-      } else if ( (redAlive && !blueAlive) || countRed > countBlue) {
-        stateObj.gameCount++;
-        this.gameOfLife.cellAlive(cell, 'red');
-      } else if ( (!redAlive && blueAlive) || countRed < countBlue) {
-        stateObj.gameCount++;
-        this.gameOfLife.cellAlive(cell, 'blue');
-      }
+  processNewState: function(cell) {
+    console.log('.....entering processNewState.......');
+    let NewStateObj = {
+      countRed: this.stateObj.redColor[cell.id] || 0,
+      countBlue: this.stateObj.blueColor[cell.id] || 0,
+      redAlive: cell.dataset.status !== 'dead' && cell.className === 'red',
+      blueAlive: cell.dataset.status !== 'dead' && cell.className === 'blue',
+      stable: true,
+      countTotal: 0
+    };
+    NewStateObj.countTotal = NewStateObj.countRed + NewStateObj.countBlue;
+    console.log('---setup variables---');
+    console.log('countRed', NewStateObj.countRed);
+    console.log('countBlue', NewStateObj.countBlue);
+    console.log('countTotal', NewStateObj.countTotal);
+    console.log('---------------------');
+    console.log('redAlive', NewStateObj.redAlive);
+    console.log('blueAlive', NewStateObj.blueAlive);
+    console.log('---------------------');
+
+    if (cell.dataset.status === 'alive') {
+      this.aliveNewState(cell, NewStateObj);
     } else {
-      if (countRed === 3 && countBlue === 0) {
-        stateObj.gameCount++;
-        this.gameOfLife.cellAlive(cell, 'red');
-      } else if (countBlue === 3 && countRed === 0) {
-        stateObj.gameCount++;
-        this.gameOfLife.cellAlive(cell, 'blue');
-      }
+      this.deadNewState(cell, NewStateObj);
+    }
+    console.log('-------back from--------------');
+    console.log('cell coming out: ', cell);
+    console.log('count coming out: ', this.stateObj.gameCount);
+    console.log('.....leaving processNewState.......');
+    console.log('---------------------');
+    console.log('\n');
+  },
+
+  aliveNewState: function(cell, NewStateObj) {
+    console.log('.....entering aliveNewState.......');
+    if (NewStateObj.countTotal > 3 || (NewStateObj.countRed < 2 && NewStateObj.countBlue < 2)) {
+      console.log('------either---------------');
+      console.log('------countTotal >3 or both red&blue count <2-------');
+      this.stateObj.gameCount++;
+      this.setStatus(cell, 'dead', 'dead');
+    } else if ( (NewStateObj.redAlive && !NewStateObj.blueAlive) || NewStateObj.countRed > NewStateObj.countBlue) {
+      console.log('------either---------------');
+      console.log('------red is alive or red count > blue count-------');
+      this.stateObj.gameCount++;
+      this.cellAlive(cell, 'red');
+    } else if ((!NewStateObj.redAlive && NewStateObj.blueAlive) || NewStateObj.countRed < NewStateObj.countBlue) {
+      console.log('------either---------------');
+      console.log('------blue is alive or blue count > red count-------');
+      if (!NewStateObj.blueAlive) this.stateObj.gameCount++;
+      this.setStatus(cell, 'alive', 'blue');
     }
   },
 
-  step: function () {
-    console.log('in step function.........');
-    let stateObj = {blueOrganism: {}, redOrganism: {}, gameCount: 0};
-    this.processTable(gameOfLife.processRules, stateObj);
-    this.processTable(gameOfLife.processNewState, stateObj);
-    console.log('redOrganism: ', stateObj.redOrganism);
-    console.log('blueOrganism: ', stateObj.blueOrganism);
-    console.log('gameCount: ', stateObj.gameCount);
-    if (!stateObj.gameCount) {
-      let game = document.getElementById('game_over');
-      game.className = '';
-      this.enableAutoPlay();
+  deadNewState: function(cell, NewStateObj) {
+    console.log('.....entering deadNewState.......');
+    if (NewStateObj.countRed === 3 && NewStateObj.countBlue === 0) {
+      console.log('------red count = 3, blue count= 0-------');
+      this.stateObj.gameCount++;
+      this.cellAlive(cell, 'red');
+    } else if (NewStateObj.countBlue === 3 && NewStateObj.countRed === 0) {
+      console.log('------blue count = 3, red count= 0-------');
+      this.stateObj.gameCount++;
+      this.setStatus(cell, 'alive', 'blue');
+    } else {
+      this.setStatus(cell, 'dead', 'dead');
     }
   },
 
   enableAutoPlay: function () {
-    console.log('clicked Auto-Play.........')
-    this.intervalId = setInterval('gameOfLife.step()', this.stepInterval);
+    console.log('.entering ...clicked Auto-Play.........')
+    console.log(this)
+    this.intervalId = setInterval(this.step(), this.stepInterval);
+    console.log('.leaving ...clicked Auto-Play.........')
   },
 
   pause: function() {
-    console.log('in pause.........')
+    console.log('...entering pause.........')
     clearInterval(this.intervalId);
-    this.intervalId = null;
-  },
-
-  newGame: function() {
-    console.log('...entering new game..........');
-    let gameOver = document.getElementById('game_over');
-    gameOver.className = 'hidden disabled';
-    let board = document.getElementById('board');
-    while (board.lastChild) {
-      board.removeChild(board.lastChild);
-    }
-    gameOfLife.intervalId = null;
-    this.createAndShowBoard();
-    console.log('...leaving new game..........');
+    this.resetState();
+    console.log('...leavinging pause.........')
   },
 
   createRandom: function() {
+    console.log('...entering create random................');
     let gameOver = document.getElementById('game_over');
     gameOver.className = 'hidden disabled';
     let board = document.getElementById('board');
@@ -168,45 +206,44 @@ var gameOfLife = {
     }
     this.createAndShowBoard();
     this.randomStart();
+    console.log('...leavinging create random................');
   },
 
   randomStart: function() {
-    console.log('creating random board............')
-  },
-
-  advancedSav: function(){
-    console.log('advanced play..Sav.......');
-    let advSettings = document.getElementById('control_panel_input');
-    advSettings.className = 'hidden disabled';
-    let advBtn = document.getElementById('control_panel_settings_default');
-    advBtn.className = '';
-    let advChg = document.getElementById('control_panel_settings_chg');
-    advChg.className = 'hidden disabled';
+    console.log('....entering random start............')
+    console.log('....leavinging random start............')
   },
 
   advancedDef: function(){
-    console.log('advanced play...Default......');
+    console.log('....entering advanced play...Default......');
       let advSettings = document.getElementById('control_panel_input');
       advSettings.className = '';
       let advBtn = document.getElementById('control_panel_settings_default');
       advBtn.className = 'hidden disabled';
       let advChg = document.getElementById('control_panel_settings_chg');
       advChg.className = '';
+    console.log('....leavinging advanced play...Default......');
   },
 
-  buildTable: function() {
-    let goltable = document.createElement('tbody');
-    // build Table HTML
-    var tablehtml = '';
-    for (var ht = 0; ht < this.height; ht++) {
-      tablehtml += "<tr id='row+" + ht + "'>";
-      for (var wd = 0; wd < this.width; wd++) {
-        tablehtml += "<td data-status='dead' id='" + ht + '-' + wd + "'></td>";
-      }
-      tablehtml += '</tr>';
-    }
-    goltable.innerHTML = tablehtml;
-    return goltable;
+  //Advanced settings listeners:
+
+  selectColor: function() {
+    this.stateObj.ColorId = document.getElementById('selectColor').value;
+  },
+
+  advSettings: function() {
+    console.log('you hit the accept advanced settings button');
+    let advSettings = document.getElementById('control_panel_input');
+    advSettings.className = 'hidden disabled';
+    let advBtn = document.getElementById('control_panel_settings_default');
+    advBtn.className = '';
+    let advChg = document.getElementById('control_panel_settings_chg');
+    advChg.className = 'hidden disabled';
+
+    console.log('did you change the color?');
+    console.log(this.stateObj.entryColorId);
+
+    console.log('...leavinging advanced play..Sav.......');
   }
 };
 
